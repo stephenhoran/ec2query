20c7697f84e28907e2f432968241fc11a2634ce4
Steve <steve.horan@theatsgroup.com>
2018-07-03 09:48:33 -0400

    Reverting buffer channel

--- apis/instances.go
+++ apis/instances.go
@@ -23,15 +23,15 @@ type Ec2instance struct {
 // to query all of the instances in a region and build on our Ec2instance struct
 func GetInstances(regions *ec2.DescribeRegionsOutput, instances *[]Ec2instance) {
 	// Iterate over our list of regions and use aws.StringValue to print the region name.
-	c := make(chan Ec2instance, 20)
+	c := make(chan Ec2instance)
 	for _, region := range regions.Regions {
 		go func(region *ec2.Region) {
 			fmt.Println("Starting new go function with region: " + aws.StringValue(region.RegionName))
 			queryInstances(aws.StringValue(region.RegionName), c)
 		}(region)
 	}
-	for range c {
-		fmt.Println(c)
+	for n := range c {
+		fmt.Println(n)
 	}
 
 	close(c)
