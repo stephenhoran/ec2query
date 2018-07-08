5263620f7d5ec919e953db7a92b02b1fe34c15e2
todd <tkeech1@hotmail.com>
2018-07-08 09:03:33 -0400

    added tests, circleci

--- apis/regions.go
+++ apis/regions.go
@@ -2,11 +2,15 @@ package apis
 
 import "github.com/aws/aws-sdk-go/service/ec2"
 
+type DescribeRegioner interface {
+	DescribeRegions(*ec2.DescribeRegionsInput) (*ec2.DescribeRegionsOutput, error)
+}
+
 // GetRegions will return a DescribeRegionsOutput stuct containing our
 // slice of regions.
 //
 // It requires that you pass an already created session.
-func GetRegions(sess *ec2.EC2) *ec2.DescribeRegionsOutput {
+func GetRegions(sess DescribeRegioner) *ec2.DescribeRegionsOutput {
 	result, err := sess.DescribeRegions(nil)
 	if err != nil {
 		panic(err)
